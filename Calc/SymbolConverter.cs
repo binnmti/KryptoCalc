a238258c6ca957941c2c56fb78600a16b73b8d34
bin matsui <BinMatsui@hotmail.com>
2023-05-07 15:51:45 +0900

    Refact

--- Calc/SymbolConverter.cs
+++ Calc/SymbolConverter.cs
@@ -7,20 +7,23 @@ public static class SymbolConverter
         if (string.Compare(nowSymbol, "yen", true) == 0)
         {
             symbolItems.TryGetValue(newSymbol, out decimal newPrice);
-            return price / newPrice;
+            return Round(price / newPrice);
         }
         else
         {
             symbolItems.TryGetValue(nowSymbol, out decimal nowPrice);
             if (string.Compare(newSymbol, "yen", true) == 0)
             {
-                return price * nowPrice;
+                return Round(price * nowPrice);
             }
             else
             {
                 symbolItems.TryGetValue(newSymbol, out decimal newePrice);
-                return price * nowPrice / newePrice;
+                return Round(price * nowPrice / newePrice);
             }
         }
     }
+
+    private static decimal Round(decimal price)
+        => Math.Round(price, 20);
 }
