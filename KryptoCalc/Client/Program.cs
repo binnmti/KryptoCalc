67097fc8903e15255888051ff4818cfad39b5814
bin matsui <BinMatsui@hotmail.com>
2023-05-07 16:24:33 +0900

    Keyborad

--- KryptoCalc/Client/Program.cs
+++ KryptoCalc/Client/Program.cs
@@ -1,11 +1,12 @@
 using KryptoCalc.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddHotKeys2();
 await builder.Build().RunAsync();
