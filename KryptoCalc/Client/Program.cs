243f4737b92f5d321a1e71ac089fc3daef4dc269
bin matsui <BinMatsui@hotmail.com>
2023-05-12 23:36:24 +0900

    cookie

--- KryptoCalc/Client/Program.cs
+++ KryptoCalc/Client/Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ICookie, Cookie>();
 builder.Services.AddHotKeys2();
 await builder.Build().RunAsync();
