4d8fe7f88f9cacfa05b2b8f4e9a73ad9450038cc
bin matsui <BinMatsui@hotmail.com>
2023-05-15 23:13:53 +0900

    Refact

--- KryptoCalc/Shared/CoinMarketsConvert.cs
+++ KryptoCalc/Shared/CoinMarketsConvert.cs
@@ -3,16 +3,13 @@
 public static class CoinMarketConvert
 {
     public static IEnumerable<CoinMarketView> ToCoinMarketView(this List<CoinMarkets> coinMarkets, string newSymbol, decimal price)
-    {
-        var newPrice = (decimal)coinMarkets.Single(x => x.Id == newSymbol).CurrentPrice;
-        return coinMarkets
+        => coinMarkets
             .Select(x => new CoinMarketView(
                 x.Id,
                 x.Name,
                 x.Symbol,
                 x.Image,
-                GetCoinPrice(price, newSymbol, newPrice, x.Id, (decimal)x.CurrentPrice)));
-    }
+                GetCoinPrice(price, newSymbol, (decimal)coinMarkets.Single(x => x.Id == newSymbol).CurrentPrice, x.Id, (decimal)x.CurrentPrice)));
 
     private static decimal GetCoinPrice(decimal price, string newSymbol, decimal newPrice, string currentSymbol, decimal currentPrice)
         => (newSymbol == "yen")
