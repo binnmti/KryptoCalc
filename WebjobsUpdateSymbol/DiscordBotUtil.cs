90c683e3f7767224e3538d132ad996a97e698dac
bin matsui <BinMatsui@hotmail.com>
2023-07-05 23:31:48 +0900

    Discord Bot Message Change

--- WebjobsUpdateSymbol/DiscordBotUtil.cs
+++ WebjobsUpdateSymbol/DiscordBotUtil.cs
@@ -11,6 +11,12 @@ internal static class DiscordBotUtil
         await client.LoginAsync(TokenType.Bot, botToken);
         await client.StartAsync();
         var channel = await client.GetChannelAsync(channelId) as IMessageChannel;
-        await channel!.SendMessageAsync(message);
+        var embed = new EmbedBuilder()
+              .WithTitle("失敗連絡")
+              .WithUrl("https://kryptocalc.scm.azurewebsites.net/azurejobs")
+              .WithColor(Color.Red)
+              .WithDescription(message)
+              .Build();
+        await channel!.SendMessageAsync(null, false, embed);
     }
 }
