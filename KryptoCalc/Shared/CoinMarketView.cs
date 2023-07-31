4cd7c4e3e40e8dcac1a840619efa25aaa0e7683e
bin matsui <BinMatsui@hotmail.com>
2023-08-01 06:59:34 +0900

    Refactor

--- KryptoCalc/Shared/CoinMarketView.cs
+++ KryptoCalc/Shared/CoinMarketView.cs
@@ -14,4 +14,25 @@ public static class CoinMarketViewExtention
 {
     public static bool CompareId(this CoinMarketView coinMarkets, string dst)
         => string.Compare(coinMarkets.Id, dst, true) == 0;
+
+    //TODO:UnitTest
+    public static IEnumerable<CoinMarketView> UpdateCoinMarketViews(this List<CoinMarketView> coinMarkets, CoinMarketView current, decimal inputNumber)
+        => coinMarkets.Select(x => new CoinMarketView(
+                x.Id,
+                x.Symbol,
+                x.Name,
+                x.Image,
+                x.CurrentPrice,
+                GetInputPrice(x, current, inputNumber),
+                x.IsLegal));
+
+    private static decimal GetInputPrice(CoinMarketView coinMarketView, CoinMarketView current, decimal inputNumber)
+        => current.IsLegal
+            ? Round(inputNumber / coinMarketView.CurrentPrice)
+            : coinMarketView.IsLegal
+                ? Round(inputNumber * current.CurrentPrice)
+                : Round(inputNumber * current.CurrentPrice / coinMarketView.CurrentPrice);
+
+    private static decimal Round(decimal price)
+        => Math.Round(price, 8);
 }
--- KryptoCalc/Shared/CoinMarketsConvert.cs
+++ KryptoCalc/Shared/CoinMarketsConvert.cs
@@ -9,25 +9,6 @@ public static class CoinMarketConvert
             x.Name,
             x.Image,
             (decimal)x.CurrentPrice,
-            0, false));
-
-    public static IEnumerable<CoinMarketView> UpdateCoinMarketViews(this List<CoinMarketView> coinMarkets, CoinMarketView current, decimal inputNumber)
-        => coinMarkets.Select(x => new CoinMarketView(
-                x.Id,
-                x.Symbol,
-                x.Name,
-                x.Image,
-                x.CurrentPrice,
-                GetInputPrice(x, current, inputNumber),
-                x.IsLegal));
-
-    private static decimal GetInputPrice(CoinMarketView coinMarketView, CoinMarketView current, decimal inputNumber)
-        => current.IsLegal
-            ? Round(inputNumber / coinMarketView.CurrentPrice)
-            : coinMarketView.IsLegal
-                ? Round(inputNumber * current.CurrentPrice)
-                : Round(inputNumber * current.CurrentPrice / coinMarketView.CurrentPrice);
-
-    private static decimal Round(decimal price)
-        => Math.Round(price, 8);
+            0,
+            false));
 }
