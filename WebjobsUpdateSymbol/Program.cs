29af11e8a54086b7cfb8205e785611a6ee65b077
bin matsui <BinMatsui@hotmail.com>
2023-10-01 20:35:58 +0900

    scraping

--- WebjobsUpdateSymbol/Program.cs
+++ WebjobsUpdateSymbol/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Text;
+using System.Transactions;
 using WebjobsUpdateSymbol;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -31,56 +32,57 @@ catch(Exception ex)
 
 async Task Run()
 {
-    using var sqlConnection = new SqlConnection(connectionString);
-    sqlConnection.Open();
-    using var transaction = sqlConnection.BeginTransaction();
-    var exceptionList = new List<string>();
-
     var coinMarketList = await CoinGeckoUtil.GetCoinMarketListAsync(httpClient);
     var priceList = await CoinGeckoUtil.GetPriceListAsync(httpClient, 
         coinMarketList.Where(x => x.MarketCapRank != 0).Select(x => x.Id).ToList());
-    int co = 1;
-    foreach (var coinMarket in coinMarketList)
+
+    using (var transaction = new TransactionScope(TransactionScopeOption.Required, TimeSpan.FromHours(2)))
     {
-        try
+        using var sqlConnection = new SqlConnection(connectionString);
+        sqlConnection.Open();
+        var exceptionList = new List<string>();
+        int co = 1;
+        foreach (var coinMarket in coinMarketList)
         {
-            sqlConnection.InsertOrUpdate(coinMarket, coinMarket.Id);
+            try
+            {
+                sqlConnection.InsertOrUpdate(coinMarket, coinMarket.Id);
+            }
+            catch (Exception ex)
+            {
+                var values = string.Concat(string.Join(",", coinMarket.GetType().GetProperties().Select(x => x.GetValue(coinMarket).ToString())));
+                var value = values + Environment.NewLine + ex.Message + Environment.NewLine + ex.StackTrace;
+                if (exceptionList.Any() && exceptionList[^1] == ex.Message) throw;
+                Console.WriteLine(value);
+                exceptionList.Add(ex.Message);
+            }
+            Console.WriteLine($"CoinMarket:{co++}/{coinMarketList.Count}:{coinMarket.Id}");
+            Thread.Sleep(1);
         }
-        catch (Exception ex)
+        co = 1;
+        foreach (var price in priceList)
         {
-            var values = string.Concat(string.Join(",", coinMarket.GetType().GetProperties().Select(x => x.GetValue(coinMarket).ToString())));
-            var value = values + Environment.NewLine + ex.Message + Environment.NewLine + ex.StackTrace;
-            if (exceptionList.Any() && exceptionList[^1] == value) throw;
-            Console.WriteLine(value);
-            exceptionList.Add(value);
+            try
+            {
+                sqlConnection.Insert(price);
+            }
+            catch (Exception ex)
+            {
+                var values = string.Concat(string.Join(",", price.GetType().GetProperties().Select(x => x.GetValue(price).ToString())));
+                var value = values + Environment.NewLine + ex.Message + Environment.NewLine + ex.StackTrace;
+                if (exceptionList.Any() && exceptionList[^1] == ex.Message) throw;
+                Console.WriteLine(value);
+                exceptionList.Add(ex.Message);
+            }
+            Console.WriteLine($"Price:{co++}/{priceList.Count}:{price.CoinMarketsId}");
+            Thread.Sleep(1);
         }
-        Console.WriteLine($"CoinMarket:{co++}/{coinMarketList.Count}:{coinMarket.Id}");
-        Thread.Sleep(1);
+        transaction.Complete();
+        if (exceptionList.Any())
+        {
+            throw new Exception("Exception!!");
+        }
     }
-    co = 1;
-    foreach (var price in priceList)
-    {
-        try
-        {
-            sqlConnection.Insert(price);
-        }
-        catch (Exception ex)
-        {
-            var values = string.Concat(string.Join(",", price.GetType().GetProperties().Select(x => x.GetValue(price).ToString())));
-            var value = values + Environment.NewLine + ex.Message + Environment.NewLine + ex.StackTrace;
-            if (exceptionList.Any() && exceptionList[^1] == value) throw;
-            Console.WriteLine(value);
-            exceptionList.Add(value);
-        }
-        Console.WriteLine($"Price:{co++}/{priceList.Count}:{price.CoinMarketsId}");
-        Thread.Sleep(1);
-    }
-    if (exceptionList.Any())
-    {
-        transaction.Rollback();
-        throw new Exception("Exception!!");
-    }
-    transaction.Commit();
 
     //using var client = new CosmosClient(cosmosEndpoint, cosmosKey);
     //Database database = await client.CreateDatabaseIfNotExistsAsync("KryptoCalcDB");
