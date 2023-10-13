078e9f88fa965683388c99cef5ba99dc0624783d
bin matsui <BinMatsui@hotmail.com>
2023-10-13 22:55:35 +0900

    comment delete

--- KryptoCalc/Server/Controllers/CoinsController.cs
+++ KryptoCalc/Server/Controllers/CoinsController.cs
@@ -22,7 +22,6 @@ public class CoinsController : ControllerBase
         var skip = (Math.Max(page, 1) - 1) * count;
         var take = count == -1 ? int.MaxValue : count;
         return _context.CoinMarketView.OrderBy(x => x.MarketCapRank).Skip(skip).Take(take);
-        //return GetCoinMarkets(skip, take);
     }
 
     [Route("/coinMarketsCount")]
